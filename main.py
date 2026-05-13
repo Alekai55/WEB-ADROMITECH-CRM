cb0ed9322266170c6b5fb17649b6942200c15e4b
Alekai55 <alejandro50.adp@gmail.com>
2026-05-13 18:03:43 +0200

    agregados archivos de python automáticos

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "¡Hola desde Flask!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
